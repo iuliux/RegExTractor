5fe7cfd1b59ad76c9e61d5f8fa82fe756e0c76cf
Iulius Curt <iulius.curt@gmail.com>
2020-03-05 12:52:02 +0200

    Minor formatting

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-'''
+"""
 RegExTractor
 ------------
 
-Python regex extractor.
+Python regex generator.
 
 Takes 2 or more strings (or even a single one) and generates a RegEx that
 matches similar strings.
 The generated RegEx always matches the original strings, but it also
 generalizes, usually matching more.
 
-Iulius Curt, 2014
-'''
+"""
 
 from subseq_tree import gen_tree, tree_to_regex, tree_to_HTML
 
 
 def extract(strs):
-    ''' (The main function)
+    """ (The main function)
     Takes a list of strings and generates a RegEx that matches similar strings.
-    '''
+    """
     tree = gen_tree(strs)
-    # import pprint
-    # pp = pprint.PrettyPrinter()
-    # pp.pprint(tree)
+    import pprint
+    pp = pprint.PrettyPrinter()
+    pp.pprint(tree)
     return tree_to_regex(tree)
 
 def extract_HTML(strs):
